afa22512e7e588e803c5595fba47f53f24c2785e
poteitoo <1105tigery@gmail.com>
2024-07-14 01:52:57 +0000

    add grpc client

--- hellogrpc/src/app.module.ts
+++ hellogrpc/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { Transport, ClientsModule } from '@nestjs/microservices';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SAMPLE_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          url: 'localhost:5001',
+          package: 'sample',
+          protoPath: join(__dirname, '../proto/sample.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- hellogrpc/src/app.service.ts
+++ hellogrpc/src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Inject } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { AppServiceClient, SampleData, SampleDataById } from './sample';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private sampleService: AppServiceClient;
+
+  constructor(@Inject('SAMPLE_PACKAGE') private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.sampleService = this.client.getService<AppServiceClient>('AppService');
+  }
+
+  getSampleData(): Observable<SampleData> {
+    return this.sampleService.findOne({ id: 1 } as SampleDataById);
   }
 }
